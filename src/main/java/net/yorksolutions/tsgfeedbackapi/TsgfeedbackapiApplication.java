ac538f01df62c2d8d41520007771a351ca73abbf
Josh Lee <jleec@yorksolutions.net>
2025-11-10 16:46:04 -0600

    added datasource and server port to the application yml, added a test topic

--- src/main/java/net/yorksolutions/tsgfeedbackapi/TsgfeedbackapiApplication.java
+++ src/main/java/net/yorksolutions/tsgfeedbackapi/TsgfeedbackapiApplication.java
@@ -1,7 +1,9 @@
 package net.yorksolutions.tsgfeedbackapi;
 
+import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TsgfeedbackapiApplication {
@@ -10,4 +12,8 @@ public class TsgfeedbackapiApplication {
         SpringApplication.run(TsgfeedbackapiApplication.class, args);
     }
 
+//    @Bean
+//    public NewTopic newTopic() {
+//        return new NewTopic("testTopic1", 1, (short) 1);
+//    }
 }
