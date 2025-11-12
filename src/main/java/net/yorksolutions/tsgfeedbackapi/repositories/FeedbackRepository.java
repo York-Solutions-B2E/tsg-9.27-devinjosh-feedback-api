338a5d66d757fda83ea228eecbaeacf2f9911282
Josh Lee <jleec@yorksolutions.net>
2025-11-12 11:33:07 -0600

    Fix FeedbackRepository bug

--- src/main/java/net/yorksolutions/tsgfeedbackapi/repositories/FeedbackRepository.java
+++ src/main/java/net/yorksolutions/tsgfeedbackapi/repositories/FeedbackRepository.java
@@ -3,9 +3,10 @@ package net.yorksolutions.tsgfeedbackapi.repositories;
 import net.yorksolutions.tsgfeedbackapi.repositories.entities.FeedbackEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface FeedbackRepository extends JpaRepository<FeedbackEntity, UUID> {
-    public FeedbackEntity findByMemberId(String memberId);
+    List<FeedbackEntity> findByMemberId(String memberId);
 }
 
