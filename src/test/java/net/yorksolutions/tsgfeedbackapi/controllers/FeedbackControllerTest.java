50d34b476bd1696bedc0a736a975487baec76142
Josh Lee <jleec@yorksolutions.net>
2025-11-14 08:30:58 -0600

    Add invalid body test

--- src/test/java/net/yorksolutions/tsgfeedbackapi/controllers/FeedbackControllerTest.java
+++ src/test/java/net/yorksolutions/tsgfeedbackapi/controllers/FeedbackControllerTest.java
@@ -2,30 +2,30 @@ package net.yorksolutions.tsgfeedbackapi.controllers;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import kafka.utils.Json;
 import net.yorksolutions.tsgfeedbackapi.dtos.FeedbackRequest;
 import net.yorksolutions.tsgfeedbackapi.dtos.FeedbackResponse;
 import net.yorksolutions.tsgfeedbackapi.services.FeedbackService;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentMatchers;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 
 import java.time.Instant;
 import java.util.UUID;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @WebMvcTest(controllers = FeedbackController.class)
@@ -51,20 +51,44 @@ public class FeedbackControllerTest {
         when(feedbackService.createFeedback(any(FeedbackRequest.class)))
                 .thenReturn(response);
         //Act
-        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/feedback")
+        ResultActions resultActions = mockMvc.perform(post("/api/v1/feedback")
             .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(request))
 
         );
         //Assert
-        resultActions.andExpect(MockMvcResultMatchers.status().isCreated())
+        resultActions.andExpect(status().isCreated())
                 .andExpect(MockMvcResultMatchers.content().json(objectMapper.writeValueAsString(response)));
 
         verify(feedbackService).createFeedback(any(FeedbackRequest.class));
     }
 
     @Test
-    public void submitFeedback_invalidBody_ExceptionThrown() throws Exception {}
+    void submitFeedback_invalidBody_HttpMessageNotReadableExceptionThrown() throws Exception {
+        String badJson = """
+                {
+                  "memberId": "m-101",
+                  "providerName": "Dr. Phill",
+                  "rating": 4,
+                  "comment": "Cool guy.",
+                  "randomThing": "oops"
+                }
+                """;
 
+        mockMvc.perform(post("/api/v1/feedback")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(badJson))
+                .andExpect(status().isBadRequest())
+                .andExpect(result -> {
+                    Exception ex = result.getResolvedException();
+                    // Make sure Spring resolved the correct exception type
+                    assertNotNull(ex);
+                    assertInstanceOf(HttpMessageNotReadableException.class, ex, "Expected HttpMessageNotReadableException but was " + ex.getClass());
+
+                });
+
+
+        verifyNoInteractions(feedbackService);
+    }
 
 }
