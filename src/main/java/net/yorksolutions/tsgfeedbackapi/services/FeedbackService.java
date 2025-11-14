d35a4ea5056b6bb6a3fe071a56bd68cfdaee11d3
Josh Lee <jleec@yorksolutions.net>
2025-11-14 11:11:21 -0600

    FIX JPA not checking insert before kafka publish

--- src/main/java/net/yorksolutions/tsgfeedbackapi/services/FeedbackService.java
+++ src/main/java/net/yorksolutions/tsgfeedbackapi/services/FeedbackService.java
@@ -66,7 +66,7 @@ public class FeedbackService {
         FeedbackEntity entity = mapToEntity(request);
         
         // Save entity (submittedAt will be set automatically by @CreationTimestamp)
-        FeedbackEntity savedEntity = feedbackRepository.save(entity);
+        FeedbackEntity savedEntity = feedbackRepository.saveAndFlush(entity);
         
         // Map to response
         FeedbackResponse response = mapToResponse(savedEntity);
